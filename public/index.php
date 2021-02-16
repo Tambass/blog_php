f45217b10d1137a7a665815335101eefc5ad2d00
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-16 16:42:28 +0100

    mise en place des premières routes

--- public/index.php
+++ public/index.php
@@ -2,3 +2,18 @@
 require '../vendor/autoload.php';
 
 $router = new AltoRouter();
+
+// Créer une variable de direction pour éviter les répétitions
+define('VIEW_PATH', dirname(__DIR__) . '/views');
+
+// Chargement des premières Uroutes
+$router->map('GET', '/blog', function () {
+    require VIEW_PATH . '/post/index.php';
+});
+$router->map('GET', '/blog/category', function () {
+    require VIEW_PATH . '/category/show.php';
+});
+
+// Demander au router si l'url rentrée correspond aux routes
+$match = $router->match();
+$match['target']();
