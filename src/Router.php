cd6f18db2da9612097461fb338b290193584a2c3
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-17 10:05:29 +0100

    création de la class Router

---
+++ src/Router.php
@@ -0,0 +1,28 @@
+<?php
+namespace App;
+
+class Router {
+
+    /**
+     *  @var string
+     */
+    private $viewPath;
+
+    /**
+     * @var AltoRouter
+     */
+    private $router;
+
+    public function __construct(string $viewPath)
+    {
+        $this->viewPath = $viewPath;
+        $this->router = new \AltoRouter();
+    }
+
+    public function get(string $url, string $view, ?string $name = null): self
+    {
+        $this->router->map('GET', $url, $view, $name);
+        return $this;
+    }
+
+}
