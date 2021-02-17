66535eccbc018548586e18155376ec0a1d6fd2f1
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-17 09:52:02 +0100

    réorganisation du routage

--- public/index.php
+++ public/index.php
@@ -3,17 +3,9 @@ require '../vendor/autoload.php';
 
 $router = new AltoRouter();
 
-// Créer une variable de direction pour éviter les répétitions
-define('VIEW_PATH', dirname(__DIR__) . '/views');
-
-// Chargement des premières Uroutes
-$router->map('GET', '/blog', function () {
-    require VIEW_PATH . '/post/index.php';
-});
-$router->map('GET', '/blog/category', function () {
-    require VIEW_PATH . '/category/show.php';
-});
-
-// Demander au router si l'url rentrée correspond aux routes
-$match = $router->match();
-$match['target']();
+$router = new Router(dirname(__DIR__) . '/views');
+//Affichage des routes ('URL', 'CHEMIN', 'NOM DE LA PAGE')
+$router->get('/blog', 'post/index', 'blog');
+$router->get('/blog/category', 'category/show', 'category');
+//Lancer le router
+$router->run();
