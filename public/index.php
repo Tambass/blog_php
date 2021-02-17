3bbb711257fe8c6b90c8b7599199bc5f83803a97
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-17 10:57:21 +0100

    mise en place d une variable de débugage pour le temps de chargement des pages

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 require '../vendor/autoload.php';
 
+// Variable de débugage pour le temps de chargement des pages visible dans le footer
+define('DEBUG_TIME', microtime(true));
+
 $router = new App\Router(dirname(__DIR__) . '/views');
 //Affichage des routes ('URL', 'CHEMIN', 'NOM DE LA PAGE')
 $router
--- views/layouts/default.php
+++ views/layouts/default.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +8,7 @@
     <!-- ?? 'text' = valeur à rentrer à défaut si le title n'a pas été paramété dans la vue -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-<body>
+<body class="d-flex flex-column h-100">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a href="#" class="navbar-brand">Mon site</a>
     </nav>
@@ -16,5 +16,13 @@
     <div class="container mt-4">
         <?= $content ?>
     </div>
+
+    <footer class="bg-light py-4 footer mt-auto">
+        <div class="container">
+            <?php if (defined('DEBUG_TIME')): ?>
+            Page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?> ms
+            <?php endif ?>
+        </div>
+    </footer>
 </body>
 </html>
