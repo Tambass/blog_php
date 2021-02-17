35216b27af60b934634a290a0c9c42aee49b1af9
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-17 10:12:53 +0100

    création de la fonction run() pour charger le router

--- public/index.php
+++ public/index.php
@@ -5,7 +5,8 @@ $router = new AltoRouter();
 
 $router = new Router(dirname(__DIR__) . '/views');
 //Affichage des routes ('URL', 'CHEMIN', 'NOM DE LA PAGE')
-$router->get('/blog', 'post/index', 'blog');
-$router->get('/blog/category', 'category/show', 'category');
-//Lancer le router
-$router->run();
+$router
+    ->get('/blog', 'post/index', 'blog')
+    ->get('/blog/category', 'category/show', 'category')
+    //Lancer le router
+    ->run();
--- src/Router.php
+++ src/Router.php
@@ -22,6 +22,16 @@ class Router {
     public function get(string $url, string $view, ?string $name = null): self
     {
         $this->router->map('GET', $url, $view, $name);
+        
+        return $this;
+    }
+
+    public function run(): self
+    {
+        $match = $this->router->match();
+        $view = $match['target'];
+        require $this->viewPath . $view . '.php';
+
         return $this;
     }
 
