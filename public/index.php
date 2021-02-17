cc6e3d38b4e03ccb22684148b9a920fd9b48d8ff
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-17 11:07:09 +0100

    paramétrage de Whoops

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ require '../vendor/autoload.php';
 // Variable de débugage pour le temps de chargement des pages visible dans le footer
 define('DEBUG_TIME', microtime(true));
 
+// Dépendance WHOOPS pour afficher une belle page de débugage
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 $router = new App\Router(dirname(__DIR__) . '/views');
 //Affichage des routes ('URL', 'CHEMIN', 'NOM DE LA PAGE')
 $router
