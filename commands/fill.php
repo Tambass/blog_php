b90d61f0aa310941728fc8ee58252a444e8d62a4
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-18 09:04:21 +0100

    rempli la bdd avec des données aléatoires

--- commands/fill.php
+++ commands/fill.php
@@ -5,7 +5,15 @@ $pdo = new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', '', [
 ]);
 
 // Vider les tables
+$pdo->exec('SET FOREIGN_KEY_CHECKS = 0');
 $pdo->exec('TRUNCATE TABLE post_category');
 $pdo->exec('TRUNCATE TABLE post');
 $pdo->exec('TRUNCATE TABLE category');
 $pdo->exec('TRUNCATE TABLE user');
+$pdo->exec('SET FOREIGN_KEY_CHECKS = 1');
+
+for ($i = 0; $i < 50; $i++){
+    $pdo->exec("INSERT INTO post SET name='Article #$i', slug='article-$i', created_at='2021-02-11 14:00:00', content='lorem ipsum'");
+}
+
+// Exécuter la commande : php commands/fill.php (dans le terminal)
