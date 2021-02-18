45db312031fc8456383757f8d0f45b08252793bf
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-18 17:27:22 +0100

    mise en place du système de pagination

--- views/post/index.php
+++ views/post/index.php
@@ -7,7 +7,35 @@ $title = 'Mon Blog';
 $pdo = new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT 12");
+
+$page = $_GET['page'] ?? 1;
+// Filtrer les url
+if (!filter_var($page, FILTER_VALIDATE_INT)) {
+    throw new Exception('Numéro de page invalide !');
+}
+// Etre redirigé vers l'url canonique de home, pour le référencement
+if ($page === '1') {
+    header('Location: ' . $router->url('home'));
+    http_response_code(301);
+    exit();
+}
+
+// Page courrente
+$currentPage = (int)$page;
+if($currentPage <= 0) {
+    throw new Exception('Numéro de page invalide !');
+}
+// Sauvegarde du nombre d'article dans la variable $count
+$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
+// Sauvegarde du nombre de page dans la variable $pages arrondi au chiffre supérieur
+$perPage = 12;
+$pages = ceil($count / $perPage);
+if($currentPage > $pages) {
+    throw new Exception('Cette page n\'existe pas !');
+}
+// Création de l'offset
+$offset = $perPage * ($currentPage - 1);
+$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 ?>
 
@@ -19,4 +47,21 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
         <?php require 'card.php' ?>
     </div>
     <?php endforeach ?>
+</div>
+
+<div class="d-flex justify-content-between my-4">
+    <?php if($currentPage > 1): ?>
+        <?php 
+            $link = $router->url('home');
+            if($currentPage > 2) $link .= '?page=' .( $currentPage -1);
+        ?>
+        <a href="<?= $link ?>" class="btn btn-primary">
+            ❮ Page précédente
+        </a>
+    <?php endif ?>
+    <?php if($currentPage < $pages): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">
+            Page suivante ❯
+        </a>
+    <?php endif ?>
 </div>
