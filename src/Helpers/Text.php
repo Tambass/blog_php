951093dcbf6ca5c5d9a93573e1795f8f93de5ab6
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-18 15:01:57 +0100

    création d une méthode pour diminuer la taille du contenu

---
+++ src/Helpers/Text.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Helpers;
+
+class Text {
+    public static function excerpt(string $content, int $limit = 60)
+    {
+        if(mb_strlen($content) <= $limit) {
+            return $content;
+        }
+        return substr($content, 0, $limit) . '...';
+    }
+}
