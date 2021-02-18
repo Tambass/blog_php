f9caa5352874399286e9476a76bea01981baea9e
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-18 09:27:12 +0100

    récupérer le dernier enregistrement

--- commands/fill.php
+++ commands/fill.php
@@ -19,6 +19,7 @@ $pdo->exec('SET FOREIGN_KEY_CHECKS = 1');
 
 for ($i = 0; $i < 50; $i++){
     $pdo->exec("INSERT INTO post SET name='{$faker->sentence()}', slug='{$faker->slug}', created_at='{$faker->date} {$faker->time}', content='{$faker->paragraphs(rand(3,15), true)}'");
+    $pdo->lastInsertId();
 }
 
 for ($i = 0; $i < 5; $i++){
