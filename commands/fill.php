01a0b13deb15825939dd232cff84779f1a30f5b0
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-18 08:53:53 +0100

    vide les tables avant de les remplir à nouveau

--- commands/fill.php
+++ commands/fill.php
@@ -3,3 +3,9 @@
 $pdo = new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
+
+// Vider les tables
+$pdo->exec('TRUNCATE TABLE post_category');
+$pdo->exec('TRUNCATE TABLE post');
+$pdo->exec('TRUNCATE TABLE category');
+$pdo->exec('TRUNCATE TABLE user');
