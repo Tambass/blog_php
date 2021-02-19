8e89bf4501e4c04ea5ad3678bee9a39e80e96d6f
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-19 15:52:04 +0100

    affiche le nom de la catégorie dans le title du head et en titre de page

--- views/category/show.php
+++ views/category/show.php
@@ -1,1 +1,29 @@
-<h1>Ma catégorie</h1>
+<?php
+use App\Connection;
+use App\Model\Category;
+
+$id = (int)$params['id'];
+$slug = $params['slug'];
+
+$pdo = Connection::getPDO();
+$query = $pdo->prepare('SELECT * FROM category WHERE id = :id');
+$query->execute(['id' => $id]);
+$query->setFetchMode(PDO::FETCH_CLASS, Category::class);
+/** @var Category|false */
+$category = $query->fetch();
+
+if($category === false) {
+    throw new Exception('Aucune catégorie ne correspond à cet ID');
+}
+
+if($category->getSlug() !== $slug) {
+    $url = $router->url('category', ['slug' => $category->getSlug(), 'id' => $id]);
+    http_response_code(301);
+    header('Location: ' . $url);
+}
+
+$title = "Catégorie {$category->getName()}";
+
+?>
+
+<h1><?= e($title) ?></h1>
